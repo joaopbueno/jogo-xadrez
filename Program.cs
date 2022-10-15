744cf4a3dea2de4865739583cd2cefa6512d7b2e
João Paulo <mrjoao16@gmail.com>
2022-10-15 13:26:59 -0300

    Primeira Pausa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -6,7 +8,26 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            PosicaoXadrez pos = new PosicaoXadrez('a',1);
+
+            Console.WriteLine(pos);
+            //try
+            //{
+            //    Posicao p = new Posicao(3, 4);
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+
+
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+
+            //    Tela.imprimirTabuleiro(tab);
+
+            //}catch(TabuleiroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);   
+            //}
         }
     }
 }
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }  
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            Posicao = null;
+            Tab = tab;
+            Cor = cor;
+            qteMovimentos = 0;  
+        }
+    }
+}
